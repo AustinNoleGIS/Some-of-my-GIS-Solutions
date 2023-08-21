9658341f22c6d1a6a9ce52e9066bbbac109be214
AustinNoleGIS <142843455+AustinNoleGIS@users.noreply.github.com>
2023-08-21 13:49:39 -0700

    Add files via upload

---
+++ for_in_loop_two_blocks.py
@@ -0,0 +1,8 @@
+# make a for-in loop with 2 blocks of code for a list of 3 variables.
+
+pet_list = ['Dog', 'Cat', 'Mouse']
+
+for i in pet_list:
+    print(i)
+    for j in pet_list:
+        print(j)
